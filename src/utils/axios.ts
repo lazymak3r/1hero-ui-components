41cf952a6f46b448322cd57878fb1eb58a9bf7f6
Aram <aramsuqiasyan20@gmail.com>
2023-04-12 16:20:02 +0400

    feat: change baseUrl

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
+// axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'https://oneheroai-backend.herokuapp.com/api';
